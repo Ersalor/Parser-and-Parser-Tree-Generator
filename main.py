7aa8fe813f0eba58353c4a027feef2e50b009a6a
Ersalor <mehmet.z.sanli@gmail.com>
2026-04-17 21:45:28 +0300

    add gotovalue function

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
 #file_name = input("Enter the file name: ")
-file = open("grammar1.txt", "r")
+file = open("grammar1.txt", "r",encoding="utf-8")
 
 file_row = []
 dict = {}
 
 for line in file:
-    print(line.strip())
+    #print(line.strip())
     file_row.append(line.strip())
 
 print()
 
 for line in file_row:
-    if "|" in line :
-        dict[line.split("::=")[0].strip()] = line.split("::=")[1].strip().split("|")
+        dict[line.split("::=")[0].strip()] = line.split("::=")[1].strip().split(" | ")
 
-    else:
-        dict[line.split("::=")[0].strip()] = line.split("::=")[1].strip()
+print(dict)
 
+#print(list(dict.keys())[0])
 
-for key in dict:
-    print(key, ":", dict[key])
+def gotovalue(grammer_dict,key):
+      
+      if key in grammer_dict:
+            value = grammer_dict[key]
+            if type(value) != list:
+                value_list = value.split(" ")
+            elif type(value) == list:
+                value_list = value
+            for i in value_list:
+                if len(i.split(" ")) > 1:
+                    for j in i.split(" "):
+                         if j in grammer_dict:
+                              gotovalue(grammer_dict,j)
+                    if i in grammer_dict:   
+                        gotovalue(grammer_dict,i)
