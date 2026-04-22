ac74d49edec3c114dca1fb36c1ab5e462c5b4a23
Ersalor <mehmet.z.sanli@gmail.com>
2026-04-22 22:05:11 +0300

    WIP: started recursive descent parser, not fully working yet

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import functions
 #Kullanıcıdan grammer dosyası alınır
-grammar_file = input("Enter the grammar file name: ").strip()
+grammar_file = "grammar1.txt" #input("Enter the grammar file name: ").strip()
 
 #Dosya açılır,okunur ve dictionary oluşturulur
 grammer_dict=functions.grammar_to_dict(grammar_file)
 
 #Kelime bazlı mı yoksa karakter bazlı mı olduğu kontrol edilir
 is_word_based = functions.is_word_based(grammer_dict)
+start_symbol = list(grammer_dict.keys())[0]
 
 #Kullanıcıdan cümle dosyası alınır
-sentence_file = input("Enter the sentence file name: ").strip()
+sentence_file = "sentence.txt" #input("Enter the sentence file name: ").strip()
 
 #Cümle dosyası açılır ve cümleler alınır
 sentences = functions.get_sentences(sentence_file)
@@ -18,4 +19,6 @@ print(functions.tokenizate(sentences=sentences, is_word_based=is_word_based))
 
 
 #Kelime bazlı mı yoksa karakter bazlı mı olduğunu kontrol edilir
-#print(functions.is_word_based(grammer_dict))
+tokenizated_sentence=functions.tokenizate(sentences=sentences, is_word_based=is_word_based)
+index=[0]
+print(functions.parse(tokenizated_sentence=tokenizated_sentence, grammar_dict=grammer_dict, start_symbol=start_symbol,index=index))
